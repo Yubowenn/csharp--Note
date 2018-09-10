a5f23e660ae1d16cc2d646ab5296fbfd30a60c89
Yubowenn <31721238+Yubowenn@users.noreply.github.com>
2018-09-10 09:55:20 +0800

    加入了一个判断语句

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ namespace testapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is my first line.");
-            Console.WriteLine("This is my second line.");
+            int a=5;
+            if(a<5)
+            {
+                Console.WriteLine("This is my first line.");
+            }
+            else
+            {
+                Console.WriteLine("This is my second line.");
+                Console.Beep(1000,1000);//发出一个蜂鸣
+            }
+            
         }
     }
 }
